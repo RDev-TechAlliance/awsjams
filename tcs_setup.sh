a9a32a1310e0d180ea6b94ea57f4122416fa42b6
Rick Devera <44529212+rickdevera@users.noreply.github.com>
2023-09-02 09:36:40 -0500

    update tcs_setup.sh

--- tcs_setup.sh
+++ tcs_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Script file to be used to for container security
 
+#############
 #Copy latest code from tenable
 echo "Downloading..."
 curl https://www.tenable.com/downloads/api/v2/pages/tenable-cs/files/tenable.cs-cli_latest_Linux_x86_64.tar.gz --output tenable.cs-cli_latest_Linux_x86_64.tar.gz
@@ -9,8 +10,21 @@ curl https://www.tenable.com/downloads/api/v2/pages/tenable-cs/files/tenable.cs-
 echo "Extracting..."
 tar xzf tenable.cs-cli_latest_Linux_x86_64.tar.gz
 
-# make it work
+# make it an executable
 chmod +x tcs
 sudo mv tcs /usr/local/bin/
+
+############
+# Download the latest Terrascan
+$ curl -L "$(curl -s https://api.github.com/repos/tenable/terrascan/releases/latest | grep -o -E "https://.+?_Darwin_x86_64.tar.gz")" > terrascan.tar.gz
+$ tar -xf terrascan.tar.gz terrascan && rm terrascan.tar.gz
+$ sudo install terrascan /usr/local/bin && rm terrascan
+
+
+# Download the latest IaC Scanner cli
+
+curl -o accurics_linux https://downloads.accurics.com/cli/latest/accurics_linux
+chmod +x accurics_linux
+sudo mv ./accurics_linux /usr/local/bin && rm accurics_linux
 
 

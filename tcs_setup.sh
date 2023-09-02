0805e5c336719f64515fb15bbee8c0ebd13d6a81
Rick Devera <44529212+rickdevera@users.noreply.github.com>
2023-09-02 09:46:54 -0500

    update tcs_setup.sh

--- tcs_setup.sh
+++ tcs_setup.sh
@@ -3,28 +3,24 @@
 
 #############
 #Copy latest code from tenable
-echo "Downloading..."
+echo "Downloading Container Scanner..."
 curl https://www.tenable.com/downloads/api/v2/pages/tenable-cs/files/tenable.cs-cli_latest_Linux_x86_64.tar.gz --output tenable.cs-cli_latest_Linux_x86_64.tar.gz
-
-# extract it
-echo "Extracting..."
 tar xzf tenable.cs-cli_latest_Linux_x86_64.tar.gz
-
-# make it an executable
 chmod +x tcs
 sudo mv tcs /usr/local/bin/
 
 ############
 # Download the latest Terrascan
-$ curl -L "$(curl -s https://api.github.com/repos/tenable/terrascan/releases/latest | grep -o -E "https://.+?_Darwin_x86_64.tar.gz")" > terrascan.tar.gz
-$ tar -xf terrascan.tar.gz terrascan && rm terrascan.tar.gz
-$ sudo install terrascan /usr/local/bin && rm terrascan
+echo "Downloading Terrascan..."
+curl -L "$(curl -s https://api.github.com/repos/tenable/terrascan/releases/latest | grep -o -E "https://.+?_Darwin_x86_64.tar.gz")" > terrascan.tar.gz
+tar -xf terrascan.tar.gz terrascan && rm terrascan.tar.gz
+sudo install terrascan /usr/local/bin && rm terrascan
 
 
 # Download the latest IaC Scanner cli
-
+echo "Downloading Tenable Cloud Security IaC scanner..."
 curl -o accurics_linux https://downloads.accurics.com/cli/latest/accurics_linux
 chmod +x accurics_linux
-sudo mv ./accurics_linux /usr/local/bin && rm accurics_linux
+sudo install accurics_linux /usr/local/bin && rm accurics_linux
 
 
